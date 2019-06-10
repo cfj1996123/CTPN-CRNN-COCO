e512023eba7f0e39790b8ebbc17a33cdaaf44f68
cfj1996123 <624082122@qq.com>
2019-06-10 15:45:42 -0700

    Add files via upload

---
+++ crnn/run_test.sh
@@ -0,0 +1,1 @@
+python test.py --snapshot ./COCO_data_models/YOUR_MODEL_NAME --data-path ./cropped_COCO --visualize True
---
+++ crnn/run_train.sh
@@ -0,0 +1,1 @@
+python train.py --test-init True --test-epoch 5 --output-dir ./COCO_data_models --data-path cropped_COCO
